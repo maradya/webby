8560b79064484ee810b8b26aaa8bc2b51e834486
Marady <maradyalexis@gmail.com>
2021-03-27 23:46:38 -0400

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
+const dodger = document.getElementById("dodger"); 
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
 
-document.querySelector('body').appendChild(h2);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+} 
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+}); 
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+} 
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+}); 
